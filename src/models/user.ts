d9c4439e2c71d51ab8a623efc624f60aafa4c4d4
Sophie Y <javachiphi@gmail.com>
2024-02-07 10:43:45 +0800

    add user model

---
+++ src/models/user.ts
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+
+// An ts interface that describes the properties
+// that are required to create a new User
+
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+// An interface that describes the properties
+// that a User Model has
+interface UserModel extends mongoose.Model<any> {
+  build(attr: UserAttrs): any;
+}
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String, // this is a JS constructor function
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<any, UserModel>('User', userSchema);
+
+// typsecript doesnt know about the static build method on the model
+// so we use the below to tell typescript about it
+// User.build({
+//   email: 'test@c.om',
+//   password: 'pass',
+// });
+
+export { User };
