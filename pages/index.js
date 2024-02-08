3d5a6f191b658c02972b751263f93bd1d5105c65
Sophie Y <javachiphi@gmail.com>
2024-02-08 15:16:40 +0800

    get initial prop for server side rendering

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,8 @@
-const landingPage = () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+  // axios.get('/api/users/currentuser');
   return (
     <div>
       <h1>Landing Page</h1>
@@ -6,4 +10,25 @@ const landingPage = () => {
   );
 };
 
-export default landingPage;
+//server side
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === 'undefined') {
+    // We are on the server
+    // requests should be made to http://ingress-nginx.ingress-nginx.svc.cluster.local
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: req.headers,
+      }
+    );
+    return data;
+  } else {
+    // We are on the browser
+    // requests can be made with a base url of ''
+    const { data } = await axios.get('/api/users/currentuser');
+
+    return data;
+  }
+};
+
+export default LandingPage;
