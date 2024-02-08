d5554eb57a11fc79f488fa70e073fcefed62c7f7
Sophie Y <javachiphi@gmail.com>
2024-02-08 16:00:21 +0800

    show the status of sign in

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,25 @@
 import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
-  // axios.get('/api/users/currentuser');
-  return (
-    <div>
-      <h1>Landing Page</h1>
-    </div>
+  console.log('currentUser', currentUser);
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are NOT signed in</h1>
   );
 };
 
 //server side
 LandingPage.getInitialProps = async (context) => {
-  const client = buildClient(context);
-  const { data } = client.get('/api/users/currentuser');
+  const client = await buildClient(context);
 
-  return data;
+  try {
+    const { data } = await client.get('/api/users/currentuser');
+    return data; // Ensure this is an object
+  } catch (error) {
+    console.error('Error fetching current user:', error);
+    return {}; // Return an empty object in case of error
+  }
 };
 
 export default LandingPage;
