9dfc8b01bfd9ec9979af078cab0cd8f4a3f9d811
Sophie Y <javachiphi@gmail.com>
2024-02-08 16:46:02 +0800

    add links

--- components/header.js
+++ components/header.js
@@ -1,15 +1,26 @@
 import Link from 'next/link';
 
 const Header = ({ currentUser }) => {
+  const links = [
+    !currentUser && { label: 'Sign Up', href: '/auth/signup' },
+    !currentUser && { label: 'Sign In', href: '/auth/signin' },
+    currentUser && { label: 'Sign Out', href: '/auth/signout' },
+  ]
+    .filter((linkgConfig) => linkgConfig)
+    .map(({ label, href }) => {
+      return (
+        <li key={href} className="nav-item">
+          <Link href={href}>{label}</Link>
+        </li>
+      );
+    });
   return (
     <nav className="navbar navbar-light bg-light">
       <Link className="navbar-brand" href="/">
         GitTix
       </Link>
       <div className="d-flex justify-content-end">
-        <ul className="nav d-flex align-items-center">
-          {currentUser ? 'sign out' : 'signin'}
-        </ul>
+        <ul className="nav d-flex align-items-center">{links}</ul>
       </div>
     </nav>
   );
