d4baecacc6b4239f5e16a4200b5059205d21cfb7
Sophie Y <javachiphi@gmail.com>
2024-02-08 11:39:55 +0800

    set up next

---
+++ pages/banana.js
@@ -0,0 +1,9 @@
+const banana = () => {
+  return (
+    <div>
+      <h1>banana</h1>
+    </div>
+  );
+};
+
+export default banana;
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+const landingPage = () => {
+  return (
+    <div>
+      <h1>Landing Page</h1>
+    </div>
+  );
+};
+
+export default landingPage;
