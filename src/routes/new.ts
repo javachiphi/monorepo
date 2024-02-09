509dcada66edb7262cb7cbd7c58f2f0f58091a13
Sophie Y <javachiphi@gmail.com>
2024-02-09 17:19:32 +0800

    add validation check

--- src/routes/new.ts
+++ src/routes/new.ts
@@ -1,7 +1,15 @@
 import express, { Request, Response } from 'express';
-import { requireAuth, validateRequest } from '@javachiphi-tix/common';
+import {
+  requireAuth,
+  validateRequest,
+  NotFoundError,
+  OrderStatus,
+  BadRequestError,
+} from '@javachiphi-tix/common';
 import { body } from 'express-validator';
 import mongoose from 'mongoose';
+import { Order } from '../models/order';
+import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
@@ -17,6 +25,38 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
+    // Find the ticket the user is trying to order in the database
+    const { ticketId } = req.body;
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    // make sure that the ticket is not already reserved
+    // ticket reserved = ticket is associated with an order and the order status is not cancelled
+    // run query to look at all orders. find an order where the ticket is the ticket we just found *and* the orders status is *not* cancelled
+    // if we find an order from that means the ticket *is* reserved
+
+    const existingOrder = await Order.findOne({
+      ticket: ticket,
+      status: {
+        $in: [
+          OrderStatus.Created,
+          OrderStatus.AwaitingPayment,
+          OrderStatus.Complete,
+        ],
+      },
+    });
+
+    if (existingOrder) {
+      throw new BadRequestError('ticket is already reserved');
+    }
+    // calculate an expiration date for this order
+
+    // build the order and save it to the database
+
+    // publish an event saying that an order was created
+
     res.send({});
   }
 );
