5d9f4ad62d5ae2b74245d2d96a38962790e7afe3
Sophie Y <javachiphi@gmail.com>
2024-02-12 15:59:35 +0800

    call publisher when create

--- src/routes/new.ts
+++ src/routes/new.ts
@@ -11,6 +11,8 @@ import {
 import { stripe } from '../stripe';
 import { Order } from '../models/order';
 import { Payment } from '../models/payment';
+import { PaymentCreatedPublisher } from '../events/listeners/publishers/payment-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -49,7 +51,13 @@ router.post(
       });
 
       await payment.save();
-      res.status(201).send({ success: true });
+      new PaymentCreatedPublisher(natsWrapper.client).publish({
+        id: payment.id,
+        orderId: payment.orderId,
+        stripeId: payment.stripeId,
+      });
+
+      res.status(201).send({ id: payment.id });
     } catch (err) {
       console.error(err); // Log the error for debugging
       res.status(400).send({ error: 'Payment failed' });
