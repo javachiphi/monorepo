298337e4c8861d3cabcd4821a2f5ecb8ad511240
Sophie Y <javachiphi@gmail.com>
2024-02-12 16:06:08 +0800

    early return if order status is completed

--- src/events/listeners/expiration-complete-listener.ts
+++ src/events/listeners/expiration-complete-listener.ts
@@ -21,6 +21,9 @@ export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent
       throw new Error('Order not found');
     }
 
+    if (order.status === OrderStatus.Complete) {
+      return msg.ack();
+    }
     // if order status is complete, do nothing
     order.set({
       status: OrderStatus.Cancelled,
