036428c2f3bad03b6a6e38902721225682dbaf8d
Sophie Y <javachiphi@gmail.com>
2024-02-12 17:25:32 +0800

    add a table and a link

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,41 @@
-const LandingPage = ({ currentUser }) => {
-  console.log('currentUser', currentUser);
-  return currentUser ? (
-    <h1>You are signed in</h1>
-  ) : (
-    <h1>You are NOT signed in</h1>
+import Link from 'next/link';
+
+const LandingPage = ({ currentUser, tickets }) => {
+  const ticketList = tickets.map((ticket) => {
+    return (
+      <tr key={ticket.id}>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+        <td>
+          <Link href="/tickets/[ticketId]" as={`/tickets/${ticket.id}`}>
+            View
+          </Link>
+        </td>
+      </tr>
+    );
+  });
+
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className="table">
+        <thread>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Link</th>
+          </tr>
+        </thread>
+        <tbody>{ticketList}</tbody>
+      </table>
+    </div>
   );
 };
 
 //server side
 LandingPage.getInitialProps = async (context, client, currentUser) => {
-  return {};
+  const { data } = await client.get('/api/tickets');
+  return { tickets: data };
 };
 
 export default LandingPage;
