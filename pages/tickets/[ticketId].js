e53e0621826bbd4282b98093cc3eaeb4461c443c
Sophie Y <javachiphi@gmail.com>
2024-02-12 18:02:30 +0800

    add show (single tix)

--- pages/tickets/[ticketId].js
+++ pages/tickets/[ticketId].js
@@ -1,5 +1,38 @@
-const TicketShow = () => {
-  return <h1>Ticket Show</h1>;
+import useRequest from '../../hooks/use-request';
+
+const TicketShow = ({ ticket }) => {
+  console.log('ticket', ticket);
+  const { doRequest, errors } = useRequest({
+    url: 'api/orders',
+    method: 'post',
+    body: {
+      ticketId: ticket.id,
+    },
+    onSuccess: (order) => console.log(order),
+  });
+  return (
+    <div>
+      <h1>{ticket.title}</h1>
+      <h4>Price: {ticket.price}</h4>
+      {errors}
+      <button onClick={doRequest} className="btn btn-primary">
+        {' '}
+        Purchase{' '}
+      </button>
+    </div>
+  );
+};
+
+TicketShow.getInitialProps = async (context, client) => {
+  try {
+    const { ticketId } = context.query;
+    const { data } = await client.get(`/api/tickets/${ticketId}`);
+
+    return { ticket: data };
+  } catch (err) {
+    console.error('error fetching one tix', err);
+    return { ticket: [] };
+  }
 };
 
 export default TicketShow;
