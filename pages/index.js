358ec7eda769663ae7f300debb66ef2add6e03bb
Sophie Y <javachiphi@gmail.com>
2024-02-12 16:30:16 +0800

    change data fetching method

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-import buildClient from '../api/build-client';
-
 const LandingPage = ({ currentUser }) => {
   console.log('currentUser', currentUser);
   return currentUser ? (
@@ -10,16 +8,8 @@ const LandingPage = ({ currentUser }) => {
 };
 
 //server side
-LandingPage.getInitialProps = async (context) => {
-  const client = await buildClient(context);
-
-  try {
-    const { data } = await client.get('/api/users/currentuser');
-    return data; // Ensure this is an object
-  } catch (error) {
-    console.error('Error fetching current user:', error);
-    return {}; // Return an empty object in case of error
-  }
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+  return {};
 };
 
 export default LandingPage;
